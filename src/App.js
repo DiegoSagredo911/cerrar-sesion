0e3ebd3ed938f6b4e167f4d4ea7d977ea8dae523
mxziino <nicoaravenafx@gmail.com>
2023-11-02 12:09:11 -0300

    popup
    implementacion de popup para cerra sesion

--- src/App.js
+++ src/App.js
@@ -3,24 +3,45 @@ import './App.css';
 import InactivityPopup from './InactivityPopup';
 
 function App() {
+
+  const uncionClose = () => {
+    //funcion que se ejecutara para cerra sesion
+  }
   return (
+    <>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <InactivityPopup/>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+          >
           Learn React
         </a>
       </header>
     </div>
+    {
+       
+      //timerInactivity variable numerica por defecto esta en 5 que detectaria 5 minutos de inactividad
+      
+      //funcionClose crear una funcion anonima en el padre ejemplo:
+
+      //const funcionClose = () => {
+        //funcion que se ejecutara para cerra sesion
+      //}
+      
+      
+    }
+          <InactivityPopup 
+          timerInactivity={5}
+          funcionClose
+          />
+    </>
   );
 }
 
--- src/InactivityPopup.jsx
+++ src/InactivityPopup.jsx
@@ -1,16 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+import React, { useState, useEffect, Fragment } from 'react';
 
-const InactivityPopup = () => {
+const InactivityPopup = ({timerInactivity, funcionClose}) => {
   const [showPopup, setShowPopup] = useState(false);
-  const [countdown, setCountdown] = useState(10);
+  //conteo de inactividad por defecto 120
+  const [countdown, setCountdown] = useState(120);
   const [userActive, setUserActive] = useState(false);
   const [logoutRequested, setLogoutRequested] = useState(false);
 
   // Funcion para cerrar la sesion
   const logout = () => {
-    //  lógica de cierre de sesión
-    // y redirigir al usuario a la página de inicio de sesión.
-    setLogoutRequested(true);
+    funcionClose();
+    setLogoutRequested(false);
+    
   };
 
   useEffect(() => {
@@ -24,7 +26,7 @@ const InactivityPopup = () => {
         if (!logoutRequested) {
           setShowPopup(true);
         }
-      }, 5 * 60 * 1000); // 5 minutos en milisegundos
+      }, timerInactivity??5 *60* 1000); // 5 minutos en milisegundos
     };
 
     // Comienza el temporizador de inactividad cuando el componente se instancia
@@ -72,16 +74,75 @@ const InactivityPopup = () => {
     };
   }, [showPopup, countdown, logoutRequested]);
 
+  function closeModal() {
+    setCountdown(120);
+    setShowPopup(false)
+    
+  }
+
   return (
+    <>
     <div>
       {showPopup && (
         <div >
           <p>Tu sesión está a punto de expirar en {countdown} segundos.</p>
-          <button onClick={() => setShowPopup(false)}>Mantener Activo</button>
-          <button onClick={logout}>Cerrar Sesión</button>
+         
         </div>
       )}
     </div>
+
+      <Transition appear show={showPopup} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black/25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg text-center font-medium leading-6 text-gray-900"
+                  >
+                    Alerta
+                  </Dialog.Title>
+                  <div className="mt-2 text-center">
+                    <p className="text-sm text-black ">
+                    Tu sesión está a punto de expirar en {countdown} segundos.
+                    </p>
+                  </div>
+
+                  <div className="mt-4 flex justify-between">
+                  <button onClick={() => closeModal()} className="inline-flex justify-center rounded-md border-[2px] border-transparent bg-[#5E81AC] px-4 py-2 text-sm font-medium text-white hover:bg-[#81A1C1] focus:outline-none focus-visible:ring-2  focus-visible:ring-offset-2">Mantener Activo</button>
+                  <button onClick={logout} className="inline-flex justify-center rounded-md borderborder-[2px]  border-transparent bg-[#5E81AC] px-4 py-2 text-sm font-medium text-white hover:bg-[#81A1C1] focus:outline-none focus-visible:ring-2  focus-visible:ring-offset-2">Cerrar Sesión</button>
+
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    
+    </>
+    
   );
 };
 
